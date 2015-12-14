82614f90c62980c1f0c8db29bc1d510e4df0095d
Romel Pérez <ronelprhone@gmail.com>
2015-12-14 11:41:05 -0500

    refactor: rename files and add templates folder

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,20 @@ let $ = require('jquery');
 let _ = require('underscore');
 let Backbone = require('backbone');
 
-let lib1 = require('libs/lib1');
+const lib1 = require('libs/lib1');
+const src1 = require('../templates/src1.html');
 
 let app = {
-    lib1: lib1
+    lib1: lib1,
+    src1: src1
 };
 
 window.app = app;
 
 $(window).ready(function ($) {
+    var n1 = 37;
+    var n2 = 42;
+
     console.log('lib example:');
-    console.log('10 + 20 =', lib1(10, 20));
+    console.log('37 + 42 =', lib1(37, 42));
 });
