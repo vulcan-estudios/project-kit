cff6e831d8c23c18882a575443aa809e3337d6aa
Romel Pérez <ronelprhone@gmail.com>
2016-12-17 11:39:01 -0500

    update: application

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/assets'));
 
 var port = process.env.PORT || 7777;
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
     if (err) throw err;
     console.log('server running at http://127.0.0.1:' + port);
 });
