e2af5afbc84d8c96012a2b17f34b3710d797739f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 21:58:39 +0800

    Delete src directory

--- src/js/app1/app1.js
+++
@@ -1,7 +0,0 @@
-const settings = require('appSettings');
-const Collection1 = require('collections/Collection1');
-const car = require('components/car/car');
-
-$(document).foundation();
-
-console.log('App1!');
