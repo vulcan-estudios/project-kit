45a171c1169eb5a75c6f0727a5c250d6ae13a512
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 21:59:05 +0800

    Delete tasks directory

--- tasks/views.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const livereload = require('gulp-livereload');
-const config = require('./config');
-
-
-module.exports = function () {
-    gulp.src(config.views).pipe(livereload());
-};
